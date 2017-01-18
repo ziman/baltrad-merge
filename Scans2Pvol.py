697e6cb702e320b66d021620b9f9c7357fdb349e
Matus Tejiscak <ziman@functor.sk>
2017-01-18 23:20:02 +0000

    Add generate_profiles.

---
+++ Scans2Pvol.py
@@ -0,0 +1,193 @@
+#!/opt/baltrad/third_party/bin/python
+'''
+Copyright (C) 2012- Swedish Meteorological and Hydrological Institute (SMHI)
+
+This file is part of RAVE.
+
+RAVE is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RAVE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with RAVE.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+## Compiles SCANs to PVOLs. Will only use the scans located closest to the
+#  nominal (reference) time given as an argument.
+#  This modules uses multiprocessing to distribute the job among CPU cores.
+
+## @file
+## @author Daniel Michelson, SMHI
+## @date 2012-01-26
+
+import sys, os, glob, time
+import _raveio, _polarvolume
+import odim_source
+import odc_filesys
+import polar_merger
+from Proj import rd
+
+
+# @param ts string of format YYYYMMDDHHmm
+# @returns float seconds since epoch to be used with time.localtime
+def GetTime(ts):
+    YYYY, MM, DD = int(ts[:4]), int(ts[4:6]), int(ts[6:8])
+    HH, mm = int(ts[8:10]), int(ts[10:12])
+
+    return time.mktime((YYYY,MM,DD,HH,mm,0,0,0,0))
+
+
+## Read a boatload of SCAN files from a single acquitition period and generate
+# PVOLs from them, where the data closest to nominal time are chosen.
+# This will read ALL data over a 15 min period, ie. 5-minute data, but only
+# write a single PVOL for the period. Scan order is preserved only in cases
+# where data are acquired ascending or descending, not interleaved.
+# Calling this function works just as easily for several radars as it does for
+# one, although it is smarter to use multi_generate() to distribute the
+# processing load.
+# @param args tuple containing the nominal time (in seconds past epoch) and
+# a list of file strings containing input data.
+def generate(args):
+    import rave_pgf_logger
+    logger = rave_pgf_logger.rave_pgf_syslog_client()
+    seconds, files = args
+    ndt = None
+    master = {}
+    merger = polar_merger.polar_merger(15) # 15 minute interval
+
+    for fstr in files:
+        try:
+          obj = _raveio.open(fstr).object
+        except Exception, e:
+          logger.exception("Failed to open file %s"%fstr)
+          continue
+
+        src = odim_source.ODIM_Source(obj.source)
+        node = src.nod
+        angle = round(obj.elangle * rd, 1)
+
+        # If this radar isn't in the dictionary, put it there
+        if node not in master.keys():
+            master[node] = {}
+
+        # If this elevation angle isn't in the node's list, put it there
+        angles = master[node]
+
+        if angle not in angles.keys() and angle < 90.0:
+            angles[angle] = []
+
+        if angle < 90.0:
+            angles[angle].append(obj)
+
+        if ndt is None:
+            ndt = merger.create_nominal_time_str(obj.date, obj.time)
+
+    # We now have a dictionary for each site containing a dictionary of
+    # angles, where the scans are. Start organizing them...
+    ttuple = time.localtime(seconds)
+
+    #print `ttuple`
+    #sys.exit(0)
+
+    for node in master.keys():
+        #print 'NODE', node
+
+        pvol = _polarvolume.new()
+        pvol.date = ndt[:8] #time.strftime("%Y%m%d", ttuple)  # Nominal time
+        pvol.time = ndt[8:] #time.strftime("%H%M%S", ttuple)
+
+        angles = master[node].keys()
+        angles.sort()
+
+        dtime = 10e10  # Dummy initial time difference
+
+        # So for each angle
+        for angle in angles:
+            #print 'ANGLE', angle, len(master[node][angle])
+
+            scan2add = None  # Very important marker
+            lon, lat, height = None, None, None
+
+            # Loop through the list of scans, determine the one closest to
+            # nominal time
+            for scan in master[node][angle]:
+                lon = float(scan.longitude)
+                lat = float(scan.latitude)
+                height = float(scan.height)
+                DATE, TIME = scan.startdate, scan.starttime
+                scansecs = GetTime(DATE+TIME[:-2])
+                dthis = scansecs - seconds
+
+                # Sanity check for French (and other?) data
+                if (scan.nbins * scan.rscale / 1000) < scan.rstart:
+                    scan.rstart /= 1000.0
+
+                if dthis <= dtime or scan2add == None:
+                    dtime = dthis
+                    scan2add = scan
+
+            pvol.addScan(scan2add)
+            #print "%s %2.1f %s" % (scan2add.starttime,
+            #                       scan2add.elangle * rd, scan2add.source)
+
+        pvol.latitude = lat
+        pvol.longitude = lon
+        pvol.height = height
+        pvol.beamwidth = scan2add.beamwidth
+        pvol.source = scan.source
+
+        # For the purists, was this volume collected ascending or descending?
+        # This is tricker for interleaved scan strategies...
+        nrscans = pvol.getNumberOfScans()
+        first, last = pvol.getScan(0), pvol.getScan(nrscans-1)
+        if int(first.starttime) > int(last.starttime):
+            pvol.sortByElevations(0)
+
+        rio = _raveio.new()
+        rio.object = pvol
+        rio.filename = odc_filesys.MakePolarFileName(rio)
+        rio.save()
+    return rio.filename  # Only really useful with data from one radar
+
+
+## Distributes 'generate' among the available CPU cores on this machine.
+#  @param seconds float representing seconds past the epoch
+def multi_generate(seconds=None):
+    import datetime
+    import multiprocessing
+    pool = multiprocessing.Pool(8)
+
+    if seconds == None:
+        seconds = odc_filesys.GetSeconds() - odc_filesys.ACQ_DELAY
+    tt = time.localtime(seconds)
+    #tt = datetime.datetime.strptime("20161018030000","%Y%m%d%H%M%S").timetuple()
+    path = odc_filesys.GetPathFromTuple(tt)
+
+    files = glob.glob(os.path.join(path, '*scan*.h5'))
+    nodes, args = [], []
+    for fstr in files:
+        node = os.path.split(fstr)[1][:5]
+        if node not in nodes:
+            nodes.append(node)
+            nodefiles = glob.glob(os.path.join(path, node+'*scan*.h5'))
+            args.append((seconds,nodefiles))
+
+    results = []
+    r = pool.map_async(generate, args, chunksize=1, callback=results.append)
+    r.wait() # Wait on the results
+#    for i in range(len(results[0])):
+#        print nodes[i], results[0][i]
+    return len(files), results[0]
+
+
+if __name__ == "__main__":
+    done = multi_generate()
+
+
+
---
+++ generate_profiles.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python2
+#
+# The Baltrad server has Python 2.6.6.
+
+try:
+    import argparse
+except ImportError:
+    import os
+    os.system('pip install --user argparse')
+    print('\n\n** installed dependency: argparse\n   please rerun the script')
+
+import os
+import re
+import sys
+import time
+import shutil
+import datetime
+import argparse
+import logging
+import collections
+import subprocess
+from os.path import join as pjoin
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s %(message)s',
+)
+log = logging.getLogger('generate_profiles')
+
+FileInfo = collections.namedtuple('FileInfo', 'path radar ftype angle ts quantities ts_extra')
+REGEX_FI = re.compile(r'(.....)_(scan|pvol)(?:_(-?[0-9.]+))?_(\d{8}T\d{4}Z)(?:_(0x[0-9a-f]+))?(?:_(\d+))?\.h5')
+
+class ProgressMeter(object):
+    def human(self, sec):
+        result = ""
+
+        if sec > 86400:
+            result += "%dd" % (sec / 86400)
+            sec %= 86400
+
+        if sec > 3600:
+            result += "%dh" % (sec / 3600)
+            sec %= 3600
+
+        if sec > 60:
+            result += "%dm" % (sec / 60)
+            sec %= 60
+
+        if sec > 0:
+            result += "%ds" % sec
+
+        return result
+
+    def __init__(self, totalItemCount):
+        self.totalItemCount = totalItemCount
+        self.startTime = time.time()
+        self.secondsElapsed = 0.0
+        self.itemsDone = 0
+        self.last_print = None
+        
+    def nextItem(self):
+        self.secondsElapsed = int(time.time() - self.startTime)
+        self.itemsDone += 1
+
+    def secs_since_print(self, seconds):
+        if self.last_print is not None:
+            return (self.secondsElapsed - self.last_print) >= seconds
+        else:
+            return True
+
+    def __str__(self):
+        self.last_print = self.secondsElapsed
+
+        es = self.secondsElapsed % 60
+        em = self.secondsElapsed / 60
+        
+        if self.itemsDone:
+            eta = self.secondsElapsed * (self.totalItemCount - self.itemsDone) / self.itemsDone
+        else:
+            eta = 0
+        
+        pp = 100*self.itemsDone / self.totalItemCount
+        
+        return "[%d/%d, %s, %.1f/s, %d%%, ETA %s]" % (
+            self.itemsDone,
+            self.totalItemCount,
+            self.human(self.secondsElapsed),
+            float(self.itemsDone) / (self.secondsElapsed or 1),
+            pp,
+            self.human(eta),
+        )
+
+class ProgramError(Exception):
+    pass
+
+def check_output(cmd):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = p.communicate()
+    retcode = p.wait()
+    return retcode, stdout
+
+def mktmp_linked(args, tmp_name, files):
+    dirname = pjoin(args.dirname_work, tmp_name + str(os.getpid()))
+    dirname_src = pjoin(dirname, 'src')
+    dirname_dst = pjoin(dirname, 'dst')
+
+    os.makedirs(dirname_src)
+    os.makedirs(dirname_dst)
+
+    # create symlinks to inputs
+    for info in files:
+        os.symlink(os.path.abspath(info.path), pjoin(dirname_src, os.path.basename(info.path)))
+
+    return dirname_src, dirname_dst
+
+def rmtmp_linked(args, tmp_name):
+    dirname = pjoin(args.dirname_work, tmp_name + str(os.getpid()))
+    shutil.rmtree(dirname)
+
+def merge_volumes(args, files, fname_out):
+    by_quantity = collections.defaultdict(list)
+    for info in files:
+        by_quantity[info.quantities].append(info)
+
+    # XXX TODO
+    # we (arbitrarily) select the first polar volume in each group for merging
+    merge_inputs = [min(files, key=lambda f: f.ts_extra) for files in by_quantity.itervalues()]
+
+    if len(merge_inputs) <= 1:
+        log.debug('nothing to merge')
+        return
+
+    # create temporary dirs
+    dir_src, dir_dst = mktmp_linked(args, 'merge', merge_inputs)
+
+    # merge!
+    try:
+        subprocess.check_call([
+            'python',
+            args.merge_files,
+            '--indir', dir_src,
+            '--outdir', dir_dst,
+            '--type', 'pvol'
+        ])
+
+        # move the result out
+        for fname in os.listdir(dir_dst):
+            os.rename(
+                pjoin(dir_dst, fname),
+                fname_out
+            )
+    except subprocess.CalledProcessError as e:
+        log.warn('merge_files failed: %s' % e)
+
+    # clean up
+    rmtmp_linked(args, 'merge')
+
+def merge_scans(args, files, fname_out):
+    dir_src, dir_dst = mktmp_linked(args, 'merge_scans', files)
+
+    cwd = os.getcwd()
+    scans2pvol_abs = os.path.abspath(args.scans2pvol)
+    fname_out_abs = os.path.abspath(fname_out)
+
+    try:
+        os.chdir(dir_src)
+        files_before = set(os.listdir('.'))
+
+        subprocess.check_call(['python', scans2pvol_abs])
+
+        # move the result out
+        for fname in os.listdir('.'):
+            if fname in files_before:
+                continue
+
+            os.rename(fname, fname_out_abs)
+    except subprocess.CalledProcessError as e:
+        log.warn('scans2pvol failed: %s' % e)
+    finally:
+        os.chdir(cwd)
+
+    # clean up
+    rmtmp_linked(args, 'merge_scans')
+
+def merge_group(args, files, fname_out):
+    file_names = [os.path.basename(f.path) for f in files]
+
+    file_types = list(sorted(set(info.ftype for info in files)))
+
+    if file_types == ['pvol']:
+        merge_volumes(args, [info for info in files if not info.ts_extra], fname_out)
+    elif file_types == ['scan']:
+        merge_scans(args, files, fname_out)
+    elif file_types == ['pvol', 'scan']:
+        # if we've got both, work from the scans
+        merge_scans(args, [i for i in files if i.ftype == 'scan'], fname_out)
+    else:
+        raise ProgramError('unrecognised file types "%s" in %s ' % (file_types, file_names))
+
+def make_bird_profile(args, fname_pvol, fname_bird_profile):
+    retcode, output = check_output([
+        args.vol2bird,
+        fname_pvol,
+        fname_bird_profile
+    ])
+
+    if retcode != 0:
+        log.debug(output)
+        log.warn('vol2bird failed')
+
+def process_files(args, files):
+    grouped = collections.defaultdict(list)
+    for info in files:
+        grouped[(info.radar, info.ts)].append(info)
+
+    dirname_merged = pjoin(args.dirname_work, 'merged')
+    if not os.path.exists(dirname_merged):
+        os.makedirs(dirname_merged)
+
+    pm = ProgressMeter(len(grouped))
+    for (radar, ts), group_files in sorted(grouped.items(), key=lambda x: x[0]):
+        base_filename = lambda ftype: \
+            make_filename(FileInfo(
+                path=None,
+                radar=radar,
+                ftype=ftype,
+                angle=None,
+                ts=ts,
+                quantities=None,
+                ts_extra=None,
+            ))
+
+        fname_merged = pjoin(dirname_merged, base_filename('pvol'))
+        fname_bird_profile = pjoin(args.dirname_out, base_filename('vp'))
+
+        merge_group(args, group_files, fname_merged)
+        make_bird_profile(args, fname_merged, fname_bird_profile)
+
+        pm.nextItem()
+        log.info('%s %s %s' % (pm, radar, ts.strftime('%Y-%m-%d %H:%M')))
+
+def parse_filename(path, match):
+    return FileInfo(
+        path=path,
+        radar=match.group(1),
+        ftype=match.group(2),
+        angle=match.group(3) and float(match.group(3)),
+        ts=datetime.datetime.strptime(match.group(4), '%Y%m%dT%H%MZ'),
+        quantities=match.group(5),
+        ts_extra=match.group(6) and datetime.datetime.fromtimestamp(
+            int(float(match.group(6)) / 100)
+        ),
+    )
+
+def make_filename(info):
+    fields = [info.radar, info.ftype]
+
+    if info.angle:
+        fields.append('%f' % info.angle)
+
+    fields.append(
+        info.ts.strftime('%Y%m%dT%H%MZ')
+    )
+
+    if info.quantities:
+        fields.append(info.quantities)
+
+    if info.ts_extra:
+        fields.append('%s' % info.ts_extra.strftime('%s'))
+
+    return '_'.join(fields) + '.h5'
+
+def is_raw_data(info):
+    if info.ftype == 'pvol':
+        return bool(info.quantities) and not bool(info.ts_extra)
+    elif info.ftype == 'scan':
+        return not bool(info.ts_extra)
+    else:
+        raise ProgramError('unknown file type "%s": %s' % (info.ftype, info.path))
+
+def main(args):
+    files_in = []
+    for fname in os.listdir(args.dirname_in):
+        match = REGEX_FI.match(fname)
+        if not match:
+            log.warn('skipping unrecognised filename: ' + fname)
+            continue
+
+        info = parse_filename(
+            pjoin(args.dirname_in, fname),
+            match
+        )
+
+        files_in.append(info)
+
+    # filter out files that are too old
+    thresh = datetime.datetime.now() - datetime.timedelta(minutes=args.age_limit)
+    files_in = [
+        info
+        for info in files_in
+        if info.ts >= thresh    # recent enough
+           and is_raw_data(info)
+    ]
+
+    if not os.path.exists(args.dirname_work):
+        os.makedirs(args.dirname_work)
+
+    process_files(args, files_in)
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument('-a', '--age-limit', type=int, default=60, metavar='MINUTES',
+        help='skip files older than MINUTES ago [%(default)s min]')
+    ap.add_argument('--merge-files', default='merge_files', metavar='PATH',
+        help='path to merge_files')
+    ap.add_argument('--scans2pvol', default='Scans2Pvol.py', metavar='PATH',
+        help='path to Scans2Pvol.py')
+    ap.add_argument('--vol2bird', default='vol2bird', metavar='PATH',
+        help='path to vol2bird')
+
+    rn = ap.add_argument_group('required named arguments')
+    rn.add_argument('-i', dest='dirname_in', required=True, metavar='PATH',
+        help='directory to read raw data from')
+    rn.add_argument('-o', dest='dirname_out', required=True, metavar='PATH',
+        help='directory to write bird profiles to')
+    ap.add_argument('-w', dest='dirname_work', required=True, metavar='PATH',
+        help='directory for intermediate files')
+
+    main(ap.parse_args())
