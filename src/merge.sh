f3fde036f3f35f9afb15b82bb58c95f881bc5055
Matus Tejiscak <ziman@functor.sk>
2017-01-19 13:30:19 +0000

    Add an explicit age limit.

--- src/merge.sh
+++ src/merge.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# 525600 minutes = 1 year
 exec /opt/radar/baltrad-merge/generate_profiles.py \
     --merge-files /opt/radar/rave/bin/merge_files \
     --scans2pvol /opt/radar/baltrad-merge/Scans2Pvol.py \
     --vol2bird /optc/radar/vol2bird/bin/vol2bird \
+    --age-limit 525600 \
     -i "${1:-/data/in}" \
     -o "${2:-/data/out}" \
     -w "${3:-/data/work}"
