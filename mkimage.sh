fed402f3cbbe2e4dc5fac9185c14f7efddaa8e94
Matus Tejiscak <ziman@functor.sk>
2017-01-19 02:13:58 +0000

    Compact the Docker image.

--- mkimage.sh
+++ mkimage.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-docker build -t baltrad-merge .
+docker build -t baltrad-merge-uncompacted .
+
+container_id=$(docker run -d baltrad-merge-uncompacted /bin/bash)
+
+# export into TAR, import the TAR
+# this is especially important to squash the layers
+# where we installed something and then removed it
+#
+# the default command CMD needs to be put back in
+docker export $container_id \
+    | docker import \
+        --change='CMD /opt/radar/baltrad-merge/generate_profiles.py --merge-files /opt/radar/rave/bin/merge_files --scans2pvol /opt/radar/baltrad-merge/Scans2Pvol.py --vol2bird /optc/radar/vol2bird/bin/vol2bird -i /data/in -o /data/out -w /data/work' \
+        /dev/stdin \
+        baltrad-merge
+
+docker stop baltrad-merge-uncompacted
+docker rm baltrad-merge-uncompacted
