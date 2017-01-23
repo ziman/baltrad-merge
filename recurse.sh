0f9b7e73f5ec6d7fc31d362cd92df5974a253c3a
Matus Tejiscak <ziman@functor.sk>
2017-01-23 13:48:33 +0000

    Add a recursion script.

---
+++ recurse.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# mac os x doesn't have a native realpath(1)
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+recurse() {
+    run_sh="$1"
+    dir_in="$2"
+    dir_out="$3"
+    dir_work="$4"
+
+    cd "$dir_in"
+    echo "* $dir_in"
+
+    if ls *.h5 &>/dev/null; then
+        mkdir -p "$dir_out"
+        "$run_sh" "$dir_in" "$dir_out" "$dir_work"
+    fi
+
+    for subdir in */; do
+        if [ -d "$subdir" ]; then
+            recurse "$run_sh" "$dir_in/$subdir" "$dir_out/$subdir" "$dir_work"
+        fi
+    done
+}
+
+if [ -z "$3" ]; then
+    echo "usage: $0 INPUT_DIR OUTPUT_DIR WORK_DIR" >&2
+    exit 1
+fi
+
+recurse "$(dirname "$(realpath "$0")")/run.sh" "$(realpath "$1")" "$(realpath "$2")" "$(realpath "$3")"
