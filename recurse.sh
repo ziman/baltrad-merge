99107db848263197436f8bdf03d09e3897849c19
Matus Tejiscak <ziman@functor.sk>
2017-02-18 20:02:20 +0000

    Add a start-from argument.

--- recurse.sh
+++ recurse.sh
@@ -10,18 +10,23 @@ recurse() {
     local dir_in="$2"
     local dir_out="$3"
     local dir_work="$4"
+    local start_from="$5"
 
     cd "$dir_in"
     echo "* $dir_in"
 
     if ls *.h5 &>/dev/null; then
-        mkdir -p "$dir_out"
-        "$run_sh" "$dir_in" "$dir_out" "$dir_work"
+        if [[ "$dir_in" < "$start_from" ]]; then
+            echo "skipping $dir_in"
+        else
+            mkdir -p "$dir_out"
+            "$run_sh" "$dir_in" "$dir_out" "$dir_work"
+        fi
     fi
 
     for subdir in */; do
         if [ -d "$dir_in/$subdir" ]; then
-            recurse "$run_sh" "$dir_in/$subdir" "$dir_out/$subdir" "$dir_work"
+            recurse "$run_sh" "${dir_in%/}/$subdir" "${dir_out%/}/$subdir" "$dir_work" "$start_from"
         fi
     done
 }
@@ -31,4 +36,9 @@ if [ -z "$3" ]; then
     exit 1
 fi
 
-recurse "$(dirname "$(realpath "$0")")/run.sh" "$(realpath "$1")" "$(realpath "$2")" "$(realpath "$3")"
+recurse \
+    "$(dirname "$(realpath "$0")")/run.sh" \
+    "$(realpath "$1")" \
+    "$(realpath "$2")" \
+    "$(realpath "$3")" \
+    "$4"
