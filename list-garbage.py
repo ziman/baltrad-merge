94578c0bf145a9b7a85a2b3f73567ab335826bd5
Matus Tejiscak <ziman@functor.sk>
2017-05-01 14:37:23 +0100

    Add list-garbage.py.

--- list-garbage.py
+++ list-garbage.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+import os
+import re
 import argparse
 
 REGEX_FI = re.compile(r'(.....)_(scan|pvol)(?:_(-?[0-9.]+))?_(\d{8}T\d{4}Z)(?:_(0x[0-9a-f]+))?(?:_(\d+))?\.h5')
 
+def traverse(args, dname):
+    print('LIST', dname)
+    with os.scandir(dname) as items:
+        for item in items:
+            item_name = os.path.join(dname, item.name)
+
+            if item.is_dir():
+                traverse(args, item_name)
+                continue
+
+            if REGEX_FI.match(item.name):
+                print('MATCH', item_name)
+            else:
+                print('MISMATCH', item_name)
+
 def main(args):
-    print(args)
+    traverse(args, args.dir_in)
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
