8641c669f0fa744a1f1ee94b91507a0248915a4c
Matus Tejiscak <ziman@functor.sk>
2017-05-04 13:20:24 +0100

    Implement the bulk of recurse.py.

--- recurse.py
+++ recurse.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python3
 
+import os
+import logging
 import argparse
+import subprocess
+from os.path import join as pjoin
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+def recurse(args, dir_in, dir_out):
+    log.info(dir_in)
+    os.makedirs(dir_out, exist_ok=True)
+
+    h5_files_present = False
+    for entry in os.scandir(dir_in):
+        if entry.is_dir():
+            recurse(args, pjoin(dir_in, entry.name), pjoin(dir_out, entry.name))
+            continue
+
+        if entry.name.endswith('.h5'):
+            h5_files_present = True
+
+    if not h5_files_present:
+        # we recursed into subdirs
+        # nothing more to do here
+        return
+
+    # h5 files present -- run `run.sh`
+    cmd = ['./run.sh', dir_in, dir_out, args.dir_work]
+    if args.radar:
+        cmd += ['--input-filter', args.radar]
+
+    if args.dry_run:
+        log.info(' '.join(cmd))
+    else:
+        subprocess.check_call(cmd)
 
 def main(args):
-    print(args)
+    recurse(args, args.dir_in, args.dir_out)    
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -13,5 +48,9 @@ if __name__ == '__main__':
 
     ap.add_argument('--date-from', metavar="YYYY/MM/DD")
     ap.add_argument('--date-to', metavar="YYYY/MM/DD")
+    ap.add_argument('--radar', metavar="CC|CCRRR", help="two- or five-letter radar code (2x country + 3x radar)")
+
+    ap.add_argument('--dry-run', default=False, action='store_true',
+        help="don't launch docker containers, just print the plan")
 
     main(ap.parse_args())
