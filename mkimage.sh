c3b48cdf130ed621d3423eff8cfdbbdff14de88e
Matus Tejiscak <ziman@functor.sk>
2017-11-28 10:12:10 +0100

    Add container tag.

--- mkimage.sh
+++ mkimage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-docker build -t baltrad-merge-uncompacted .
+container_tag="$1"
 
-container_id=$(docker run -d baltrad-merge-uncompacted /bin/bash)
+docker build -t baltrad-merge-uncompacted"$container_tag" .
+
+container_id=$(docker run -d baltrad-merge-uncompacted"$container_tag" /bin/bash)
 
 # export into TAR, import the TAR
 # this is especially important to squash the layers
@@ -13,7 +15,7 @@ docker export $container_id \
     | docker import \
         --change='CMD /opt/radar/baltrad-merge/merge.sh' \
         /dev/stdin \
-        ziman/baltrad-merge
+        ziman/baltrad-merge"$container_tag"
 
 docker stop $container_id
 docker rm $container_id
